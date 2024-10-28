2913082a89afb2517da3bf7fd89503416175e8cf
RaeesaSirkhot <raeesasirkhot@gmail.com>
2024-10-29 01:12:51 +0200

    Improved the user experience

--- script.js
+++ script.js
@@ -13,8 +13,12 @@ function generatePoem(event) {
   let apiKey = "261aoadf0f38dt20ba85250b94b30d7a";
   let prompt = `User instrcutions: Generate a flower poem about ${instructionsInput.value}`;
   let context =
-    "You are a poem expert and love to write short flower poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a poem expert and love to write short flower poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Flower poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
